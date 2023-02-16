7ef8281c832ac7973f1f9c7cf4f8cd1d3d13e198
millifail <hwangbird99@gmail.com>
2023-02-16 16:19:02 +0900

    landing page template 생성 및 Main Card 삽입

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'c45bb23d27b8e9df84c6663f4f1ed436';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
+import MainImage from "./Sections/MainImage";
 
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  const [Movies, setMovies] = useState([]);
+  const [MainMovieImage, setMainMovieImage] = useState(null);
+
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+        setMovies([response.results]);
+        // console.log(response.results[0].backdrop_path);
+        setMainMovieImage(response.results[0]);
+      });
+  }, []);
+
+  console.log(Movies);
+  console.log(MainMovieImage);
+
+  return (
+    <div style={{ width: "100%", margin: "0" }}>
+      {/* main image */}
+      {MainMovieImage && 
+      <MainImage 
+      image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`} 
+      title={MainMovieImage.original_title}
+      description ={MainMovieImage.overview}
+      />}
+
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2>Movies by latest</h2>
+        <hr />
+
+        {/* Movie Grid Cards */}
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>Load More</button>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
